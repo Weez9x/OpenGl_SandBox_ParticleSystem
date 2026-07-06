d8eef066bc1ab5065c19b52e7e33a217e718a483
Byt3. <weezex2k18@gmail.com>
2026-07-06 16:28:25 +0500

    add glad

--- main.cpp
+++ main.cpp
@@ -1,52 +1,15 @@
-#include <glew.h>
+#include <glad/glad.h>
 #include <glfw3.h>
-#include <iostream>
 
-void error_callback(int error, const char* description) {
-    std::cerr << "Error: " << description << std::endl;
-}
+int main()
+{
+	float vertices[] = {
+		//position					//color
+	   -0.5f, -0.5f, 0.0f,	1.0f, 0.0f, 0.0f, // Bottom left
+		0.5f, -0.5f, 0.0f,	0.0f, 1.0f, 0.0f, // Bottom right
+		0.0f,  0.5f, 0.0f,	0.0f, 0.0f, 1.0f  // Top center
+	};
 
-int main() {
-    if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW" << std::endl;
-        return -1;
-    }
 
-    glfwSetErrorCallback(error_callback);
-
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Simple Example", NULL, NULL);
-    if (!window) {
-        std::cerr << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-
-    if (glewInit() != GLEW_OK) {
-        std::cerr << "Failed to initialize GLEW" << std::endl;
-        return -1;
-    }
-
-    while (!glfwWindowShouldClose(window)) {
-        glClearColor(0.1f, 0.2f, 0.2f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.0f, 0.0f, 0.0f);
-        glVertex2f(-0.5f, -0.5f);
-        glColor3f(0.0f, 1.0f, 0.0f);
-        glVertex2f(0.5f, -0.5f);
-        glColor3f(0.0f, 0.0f, 1.0f);
-        glVertex2f(0.0f, 0.5f);
-        glEnd();
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
-
-    return 0;
+	return 0;
 }
